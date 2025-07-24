15dc47418d3a84d2bc40c4dad88ad4fd0e9c74f1
harshdusane9 <hcdusane@gmail.com>
2025-07-24 18:37:42 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,21 +1,12 @@
-const socket = io();
-const output = document.getElementById('output');
-let mediaRecorder, audioChunks = [];
-
-document.getElementById('start').onclick = async () => {
-    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-    mediaRecorder = new MediaRecorder(stream);
-    mediaRecorder.start(1000);  // every second
-
-    mediaRecorder.ondataavailable = (e) => {
-        socket.emit('audio', e.data);
-    };
-};
-
-document.getElementById('stop').onclick = () => {
-    if (mediaRecorder) mediaRecorder.stop();
-};
-
-socket.on('transcript', (data) => {
-    output.innerHTML += "<br>👉 " + data;
+document.getElementById("recordBtn").addEventListener("click", () => {
+    fetch("/speech-to-text", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ audioData: "Sample Audio Data" })
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("output").innerText = data.text;
+    })
+    .catch(err => console.error(err));
 });
